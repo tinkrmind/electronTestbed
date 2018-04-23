84dcd7f6c31ee4180ba7cfe854e4f2d53fddac1a
tinkrmind <tinkrmind@gmail.com>
2018-04-23 06:11:40 +0000

    added TTS, fixed caching issue

--- frontEnd/server.js
+++ frontEnd/server.js
@@ -1,13 +1,12 @@
-
-/*
-  Four line static file server
-  This server script serves files from a subfolder called 'public'
-  adapted from expressjs.com examples, 2016
-  by Tom Igoe
-*/
 var express = require('express');	        // include the express library
 var app = express();					          // create a server using express
-app.use('/assets',express.static('assets'))
+app.use('/assets',express.static('assets', {
+  setHeaders: function(res, path) {
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+  }
+}))
 app.use('/',express.static('public')); // serve static files from /public
 app.listen(1337);                      // start the server
 
